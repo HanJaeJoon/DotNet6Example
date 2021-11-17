b6bfb4035a100b3d3fa422f52c04500fd0cdb2e8
jaejoon.han <jaejoon.han@crevisse.com>
2021-11-17 21:31:20 +0900

    Add project files.

---
+++ DotNet6Example/Models/Test.cs
@@ -0,0 +1,23 @@
+﻿namespace DotNet6Example.Models
+{
+    public class Test
+    {
+        private string _value { get; set; }
+
+        public string Value1
+        {
+
+            get
+            {
+                return _value;
+            }
+
+            set
+            {
+                _value = value;
+            }
+        }
+
+        public string Value2 { get => field; set => field = value; }
+    }
+}
